b2e6ec78256b2e3b9d1a13580ef0cf8286ae10ec
d.jiryaee <d.jiryaee@arshinco.com>
2023-05-10 12:09:20 +0430

    remove default redis_prefix

--- src/config/redis.constant.ts
+++ src/config/redis.constant.ts
@@ -1,3 +1,2 @@
 require('dotenv').config()
-const prefix = 'restulTypescriptKoa'
-export const REDIS_BLACKLIST_KEYNAME = `${process.env.REDIS_PREFIX || prefix}:jwt-blacklist` 
+export const REDIS_BLACKLIST_KEYNAME = `${process.env.REDIS_PREFIX}:jwt-blacklist` 
