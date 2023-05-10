3a8442a77e6fad30409ff33dab225c5410b27a35
d.jiryaee <d.jiryaee@arshinco.com>
2023-05-10 12:09:51 +0430

    add interface for env variable

--- src/interfaces/config.interface.ts
+++ src/interfaces/config.interface.ts
@@ -15,12 +15,35 @@ export interface IRedisConnDetails {
 
 
 export interface IConfig {
+    dbPort: number;
+    dbHost: string;
+    dbName: string;
     nodeEnv: string;
-    port: number;
+    apiPort: number;
     debugLoggin: boolean;
-    dbsslConn?: boolean;
     jwt: IJwtSecret;
     redis: IRedisConnDetails;
-    databaseUrl: string;
     dbEntitiesPath: string[]
+}
+
+export interface IEnvVariables {
+    API_PORT: IEnvType
+    NODE_ENV: IEnvType
+    JWT_SECRET: IEnvType
+    JWT_ACCESS_TOKEN_SECRET: IEnvType
+    JWT_ACCESS_TOKEN_LIFE: IEnvType
+    JWT_REFRESH_TOKEN_LIFE: IEnvType
+    JWT_REFRESH_TOKEN_SECRET: IEnvType
+    DB_NAME: IEnvType
+    DB_PORT: IEnvType
+    DB_HOST: IEnvType
+    REDIS_HOST: IEnvType
+    REDIS_PORT: IEnvType
+    REDIS_BLACKLIST_ENABLED: IEnvType
+    REDIS_PREFIX: IEnvType
+}
+
+interface IEnvType {
+    default?: any
+    required?: boolean
 }
