811cf67383443893661722f645ca93ab39a012dd
d.jiryaee <d.jiryaee@arshinco.com>
2023-05-13 15:03:29 +0330

    add pick function to utils

--- src/libraries/utils.ts
+++ src/libraries/utils.ts
@@ -1,24 +1,44 @@
 import { BaseContext } from 'koa'
+import { Context } from 'koa-swagger-decorator';
 
-export function omit(obj: any, props: string[]) {
-    const result = { ...obj };
+/**
+ * 
+ * @param {object} obj The source object.
+ * @param {...(string|string[])} keys The property paths to omit.
+ * @returns {object} Returns the new object.
+ */
+export function omit<T extends object, k extends keyof T>(obj: T, ...keys: k[]): object {
+    keys.forEach(key => {
+        delete obj[key]
+    })
+    return obj
+}
 
-    props.forEach((prop: string) => {
-        delete result[`${prop}`]
+/**
+ * 
+ * @param {object} obj  The source object
+ * @param {...(string|string[])} keys The property paths to pick.
+ * 
+ * @returns {object} Returns the new object.
+ */
+export function pick<T extends object, k extends keyof T>(obj: T, ...keys: k[]): object {
+    const ret: any = {}
+    keys.forEach(key => {
+        ret[key] = obj[key]
     })
 
-    return result;
+    return ret
 }
 
 /**
  * Applies the status code and message on ctx body and returns
  * 
- * @param  {BaseContext} context Koa context object for error handling
+ * @param  {Context} context Koa context object for error handling
  * @param  {number} status the statusCode to respond with
  * @param  {any} body the body of the response
  */
 export const response = (
-    context: BaseContext,
+    context: Context,
     status: number,
     body?: Record<string, any> | string | Array<Record<string, any>>,
 ): void => {
