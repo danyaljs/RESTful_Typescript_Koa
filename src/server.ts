54c65abf5fb6a691111034b65f0a187e745d72cf
d.jiryaee <d.jiryaee@arshinco.com>
2023-05-14 15:27:30 +0330

    add protected routes, add jwt middleware

--- src/server.ts
+++ src/server.ts
@@ -8,6 +8,8 @@ import { staticRouter } from './routes/static'
 import { config } from './config/config'
 import { unprotectedRouter } from './routes/unprotected'
 import { setupConnection } from './db/connections'
+import { protectedRouter } from './routes/protected'
+import jwt from 'koa-jwt'
 
 export const server = function (): Server {
 
@@ -24,7 +26,16 @@ export const server = function (): Server {
 
     app.use(staticRouter.routes()).use(staticRouter.allowedMethods())
 
+    // these routes are NOT protected by the JWT middleware
     app.use(unprotectedRouter.routes()).use(unprotectedRouter.allowedMethods())
+
+    // JWT middleware -> below this line routes are only reached if JWT token is valid
+    app.use(jwt({ secret: config.jwt.accessTokenSecret }).unless({ path: [/^\/assets|swagger-/] }))
+
+    // These routes are protected by the JWT middleware
+    app.use(protectedRouter.routes()).use(protectedRouter.allowedMethods())
+
+
     console.log(`Server running on port ${config.apiPort}`)
 
     return createServer(app.callback()).listen(config.apiPort)
