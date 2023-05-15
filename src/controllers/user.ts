f7235507651287005ae9b33a777a3aa3403a6a68
d.jiryaee <d.jiryaee@arshinco.com>
2023-05-15 14:33:09 +0330

    add updateUser

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,9 +1,10 @@
 import { body, Context, path, request, responses, summary } from "koa-swagger-decorator";
 import { response } from "../libraries/utils";
-import { createUserSchema, requestValidationSchema } from "../db/schemas/user";
+import { createUserSchema, requestValidationSchema, updateUserSchema } from "../db/schemas/user";
 import * as UserService from '../services/user'
 import * as ValidationService from '../services/validate'
 import { ObjectId } from 'mongodb'
+import * as AuthService from '../services/auth'
 export default class UserController {
 
     @request('post', '/users')
@@ -55,7 +56,28 @@ export default class UserController {
         await ValidationService.validateRequest(context, { _id: context.params.id }, requestValidationSchema, [
             '_id',
         ])
-        
+
         response(context, 200, await UserService.findUser(context, { _id: new ObjectId(context.params.id) }))
+    }
+
+    @request('put', '/users/{id}')
+    @summary('Update a user')
+    @path({ id: { type: 'string', required: true, description: 'id of user to update' } })
+    @body(updateUserSchema)
+    @responses({
+        200: { description: 'user updated successfully' },
+        400: { description: 'user not found, user already exists, validation errors, user already exists' },
+        401: { description: 'token authorization error' },
+        403: { description: 'not authorized to perform this action' },
+    })
+    public static async updateUser(context: Context): Promise<void> {
+        await ValidationService.validateRequest(context, context.request.body, requestValidationSchema,[])
+       
+        const userToBeUpdated = UserService.createUpdateUserModel(context)
+
+        await AuthService.verifyTokenEmail(context, userToBeUpdated)
+        await UserService.findUser(context, { _id: new ObjectId(context.params.id) })
+
+        response(context,200,(await UserService.updateUser(context,userToBeUpdated)).public())
     }
 }
